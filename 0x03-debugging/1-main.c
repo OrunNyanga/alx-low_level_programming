ed61064b5064009215ff26c3dd0059dd42eb2f99
Orun Nyanga <orunnyanga@gmail.com>
2022-04-15 18:30:28 -0700

    Lets run with action debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+
+printf("Infinite loop incoming :(\n");
+
+i = o;
+
+/*
+ *while (i < 10)
+ *{
+ *Infinite loop - No increase of variable
+ * putchar(i);
+}
+*/
+
+printtf("Infinite loop avoided! \\0/\n");
+return (0);
+}
