fff749b96de56648535613c53b3c17f5697a4d16
Orun Nyanga <orunnyanga@gmail.com>
2022-05-10 21:28:51 -0700

    Lets see how zero malloc checks out

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,31 +1,22 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 #include <stdio.h>
 
-#include <stdlib.h>
 /**
- * malloc_checked - allocates memory using malloc
- * @b: size to be allocated
- * Return: pointer to allocated memory or exit with 98
- **/
-
- * malloc_checked - allocate a space
- * @b: unsigned int
- * Return: void
+ * malloc_checked - Allocates memory
+ * @b: size to allocate
+ *
+ * Return: Nothing.
  */
 void *malloc_checked(unsigned int b)
 {
 	void *p;
-void *x;
 
 	p = malloc(b);
+
 	if (p == NULL)
 		exit(98);
+	
 	return (p);
-x = malloc(b);
-if (x == NULL)
-{
-	exit(98);
 }
-return (x);
-}
+
